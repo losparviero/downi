d85d212f1c63b02b969b085e0cf1e05c15543d3b
Zubin <me@zubin.org.in>
2023-03-19 19:59:39 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -14,82 +14,78 @@
 require("dotenv").config();
 const { Bot } = require("grammy");
 
-async function downi() {
-  // Bot
+// Bot
 
-  const bot = new Bot(process.env.BOT_TOKEN);
+const bot = new Bot(process.env.BOT_TOKEN);
 
-  // Plugins
+// Plugins
 
-  bot.use(log);
-  bot.use(responseTime);
+bot.use(log);
+bot.use(responseTime);
 
-  // Response
+// Response
 
-  async function responseTime(ctx, next) {
-    const before = Date.now();
-    await next();
-    const after = Date.now();
-    console.log(`Response time: ${after - before} ms`);
-  }
-
-  // Log
-
-  async function log(ctx, next) {
-    const from = ctx.from;
-    const name =
-      from.last_name === undefined
-        ? from.first_name
-        : `${from.first_name} ${from.last_name}`;
-    console.log(
-      `From: ${name} (@${from.username}) ID: ${from.id}\nMessage: ${ctx.message.text}`
-    );
-    await next();
-  }
-
-  // Commands
-
-  bot.command("", async (ctx) => {
-    await ctx.reply(
-      "*Bot is down till further notice.*\n_We apologise for the inconvenience caused._",
-      { parse_mode: "Markdown" }
-    );
-  });
-
-  // Messages
-
-  bot.on("message", async (ctx) => {
-    await ctx.reply(
-      "*Bot is down till further notice.*\n_We apologise for the inconvenience caused._",
-      { parse_mode: "Markdown" }
-    );
-  });
-
-  // Error
-
-  bot.catch((err) => {
-    const ctx = err.ctx;
-    console.error(`Error while handling update ${ctx.update.update_id}`);
-    const e = err.error;
-    if (e instanceof GrammyError) {
-      console.error("Error in request:", e);
-      if (e.description === "Forbidden: bot was blocked by the user") {
-        console.log("Bot was blocked by the user");
-      } else {
-        ctx.reply("An error occurred");
-      }
-    } else if (e instanceof HttpError) {
-      console.error("Could not contact Telegram:", e);
-    } else {
-      console.error("Unknown error:", e);
-    }
-  });
-
-  // Run
-  console.log(
-    "Bot started. Please keep this window running or setup persistent execution using startup managers like PM2."
-  );
-  await bot.start();
+async function responseTime(ctx, next) {
+  const before = Date.now();
+  await next();
+  const after = Date.now();
+  console.log(`Response time: ${after - before} ms`);
 }
 
-module.exports = { downi };
+// Log
+
+async function log(ctx, next) {
+  const from = ctx.from;
+  const name =
+    from.last_name === undefined
+      ? from.first_name
+      : `${from.first_name} ${from.last_name}`;
+  console.log(
+    `From: ${name} (@${from.username}) ID: ${from.id}\nMessage: ${ctx.message.text}`
+  );
+  await next();
+}
+
+// Commands
+
+bot.command("", async (ctx) => {
+  await ctx.reply(
+    "*Bot is down till further notice.*\n_We apologise for the inconvenience caused._",
+    { parse_mode: "Markdown" }
+  );
+});
+
+// Messages
+
+bot.on("message", async (ctx) => {
+  await ctx.reply(
+    "*Bot is down till further notice.*\n_We apologise for the inconvenience caused._",
+    { parse_mode: "Markdown" }
+  );
+});
+
+// Error
+
+bot.catch((err) => {
+  const ctx = err.ctx;
+  console.error(`Error while handling update ${ctx.update.update_id}`);
+  const e = err.error;
+  if (e instanceof GrammyError) {
+    console.error("Error in request:", e);
+    if (e.description === "Forbidden: bot was blocked by the user") {
+      console.log("Bot was blocked by the user");
+    } else {
+      ctx.reply("An error occurred");
+    }
+  } else if (e instanceof HttpError) {
+    console.error("Could not contact Telegram:", e);
+  } else {
+    console.error("Unknown error:", e);
+  }
+});
+
+// Run
+console.log(
+  "Bot started. Please keep this window running or setup persistent execution using startup managers like PM2."
+);
+bot.start();
